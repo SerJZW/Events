98dc0800f3ed84a7b3c52795f103d45db8cfc767
Siega <Siega@MSI>
2023-05-04 13:58:23 +0700

    Добавьте файлы проекта.

---
+++ Events/Program.cs
@@ -0,0 +1,15 @@
+﻿using Events;
+
+class Programm
+{
+    static void Main(string[] args)
+    {
+        RobotEvent robot = new RobotEvent();
+        robot.Notion += robot.BackDirection;
+        for (int i = 0; i < 10; i++)
+        {
+            robot.DefineDirection();
+        }
+
+    }
+}
---
+++ Events/RobotEvent.cs
@@ -0,0 +1,24 @@
+﻿namespace Events
+{
+    internal class RobotEvent
+    {
+        public delegate void RobotNotions();
+        public event RobotNotions? Notion;
+        private int GetDirection() => new Random().Next(3);
+        public void DefineDirection()
+        {
+            switch (GetDirection())
+            {
+                case 0: Notion?.Invoke(); break;
+                case 1: Console.WriteLine("Иду вверх"); break;
+                case 2: Console.WriteLine("Иду влево"); break;
+                case 3: Console.WriteLine("Иду вправо"); break;
+            }
+        }
+        public void BackDirection()
+        {
+            using var sw = new StreamWriter("C:/Users/zemzh/OneDrive/Рабочий стол/Daybi.txt", true, System.Text.Encoding.Default);
+            sw.WriteLine("Иду назад");
+        }
+    }
+}
